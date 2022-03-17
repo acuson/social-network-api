53ff545c6707ad362855b5dc66b1ff1bc4deaccc
Alyssa Cuson <acuson98@gmail.com>
2022-03-16 21:02:47 -0400

    added user and thought data

---
+++ utils/data.js
@@ -0,0 +1,67 @@
+const userData = [
+    {
+        username: 'allie',
+        email: 'allie@email.com',
+        thoughts: [],
+        friends: []
+    },
+    {
+        username: 'christian',
+        email: 'christian@email.com',
+        thoughts: [],
+        friends: []
+    },
+    {
+        username: 'henry',
+        email: 'henry@email.com',
+        thoughts: [],
+        friends: []
+    },
+    {
+        username: 'avery',
+        email: 'avery@email.com',
+        thoughts: [],
+        friends: []
+    },
+    {
+        username: 'mikayla',
+        email: 'mikayla@email.com',
+        thoughts: [],
+        friends: []
+    },
+    {
+        username: 'mia',
+        email: 'mia@email.com',
+        thoughts: [],
+        friends: []
+    }
+];
+
+const thoughtData = [
+    {
+        username: 'allie',
+        thoughtText: 'What am I going to do this weekend?'
+    },
+    {
+        username: 'christian',
+        thoughtText: 'Elden Ring is hard!'
+    },
+    {
+        username: 'henry',
+        thoughtText: 'ruff ruff ruff!'
+    },
+    {
+        username: 'avery',
+        thought: 'I hate Mondays!'
+    },
+    {
+        username: 'mikayla',
+        thought: 'I wonder what Mia is up to'
+    },
+    {
+        username: 'mia',
+        thought: '...'
+    }
+];
+
+module.exports = {userData, thoughtData};
