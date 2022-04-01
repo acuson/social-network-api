400ccaa99a78d78aeb555349239ee13acb3628b9
Alyssa Cuson <acuson98@gmail.com>
2022-03-31 21:49:07 -0400

    fixing mongoose connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,8 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB';
-
-connect(connectionString, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
-module.exports = connection;
+module.exports = mongoose.connection;
