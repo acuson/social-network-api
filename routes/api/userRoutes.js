0af1c29082ec35d9ec12cee244c221530f2e8f6f
Alyssa Cuson <acuson98@gmail.com>
2022-03-31 21:48:49 -0400

    adjusting routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -13,6 +13,7 @@ router.route('/').get(getUsers).post(createUser);
 
 // For /api/users/:userId
 router.route('/:userId').get(getSingleUser).delete(deleteUser);
+//Add put route to update a user
 
 // For /api/users/:userId/friends/:friendId
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
