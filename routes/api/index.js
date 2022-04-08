ce6a818c32991b25bd6a20340dba1fc7bd29a228
Alyssa Cuson <acuson98@gmail.com>
2022-04-07 21:30:28 -0400

    fixing server into index.js for routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const thoughtsRoutes = require('./thoughtsRoutes');
 
+console.log(userRoutes);
+console.log(thoughtsRoutes);
 router.use('/user', userRoutes);
 router.use('/thoughts', thoughtsRoutes);
 
