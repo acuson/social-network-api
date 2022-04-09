b70ce80dae035b22b4763fd70c3720f84bd1dbb5
Alyssa Cuson <acuson98@gmail.com>
2022-04-09 11:28:49 -0400

    fixed typo

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const thoughtsRoutes = require('./thoughtsRoutes');
 
-console.log(userRoutes);
-console.log(thoughtsRoutes);
-router.use('/user', userRoutes);
+router.use('/users', userRoutes);
 router.use('/thoughts', thoughtsRoutes);
 
 module.exports = router;
